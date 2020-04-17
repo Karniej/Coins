237b619198460c2e97f0f67ebcdd692edd5b5a7b
Paweł Karniej <karniej.p@gmail.com>
2020-04-17 09:35:02 +0200

    Initial Commit

---
+++ components/TabBarIcon.tsx
@@ -0,0 +1,15 @@
+import { Ionicons } from '@expo/vector-icons'
+import * as React from 'react'
+
+import colors from '../constants/colors'
+
+export default function TabBarIcon(props) {
+  return (
+    <Ionicons
+      name={props.name}
+      size={30}
+      style={{ marginBottom: -3 }}
+      color={props.focused ? colors.tabIconSelected : colors.tabIconDefault}
+    />
+  )
+}

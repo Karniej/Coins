187b589792fa5d99b7112e63f57bd6e3efaf5a89
Paweł Karniej <karniej.p@gmail.com>
2020-04-17 12:07:30 +0200

    Update TabBarIcon.tsx

--- components/TabBarIcon.tsx
+++ components/TabBarIcon.tsx
@@ -1,9 +1,13 @@
 import { Ionicons } from '@expo/vector-icons'
 import * as React from 'react'
-
 import colors from '../constants/colors'
 
-export default function TabBarIcon(props) {
+type Props = {
+  name: string
+  focused: boolean
+}
+
+export default function TabBarIcon(props: Props) {
   return (
     <Ionicons
       name={props.name}
